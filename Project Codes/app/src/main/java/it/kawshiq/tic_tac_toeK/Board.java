4141fb010e6fbc1c39c65f7259b0ea2eb6a7c2fb
Ariful Hasan Kawshiq <hasan.kawshiq@northsouth.edu>
2019-05-01 18:38:34 +0600

    Update Board.java

--- Project Codes/app/src/main/java/it/kawshiq/tic_tac_toeK/Board.java
+++ Project Codes/app/src/main/java/it/kawshiq/tic_tac_toeK/Board.java
@@ -13,6 +13,31 @@ public class Board {
     private static int GAME_RESULT_FAIL=2;
     private static int GAME_RESULT_DRAW=0;
 
-    
+     int[][] gameBoardArr=new int[3][3];
+
+    private int playCounter=1;
+
+    public boolean gameOver = false;
+
+    public Board copy(){
+
+        Board copy=new Board();
+
+        if (this.gameOver){
+            copy.gameOver=true;
+        }else {
+            copy.gameOver=false;
+        }
+
+        copy.playCounter=Integer.valueOf(this.playCounter);
+
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                copy.gameBoardArr[i][j]=this.gameBoardArr[i][j];
+            }
+        }
+
+        return copy;
+    }
 
 }
