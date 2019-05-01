0f10264020467c0b8881bf526e2e3a881ef8e8ed
Ariful Hasan Kawshiq <hasan.kawshiq@northsouth.edu>
2019-05-01 18:35:26 +0600

    Update Board.java

--- Project Codes/app/src/main/java/it/kawshiq/tic_tac_toeK/Board.java
+++ Project Codes/app/src/main/java/it/kawshiq/tic_tac_toeK/Board.java
@@ -1,1 +1,18 @@
 
+package it.kawshiq.tic_tac_toeK;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Board {
+
+    private static int GAME_RESULT_WIN=1;
+    private static int GAME_RESULT_FAIL=2;
+    private static int GAME_RESULT_DRAW=0;
+
+    
+
+}
