01c09d996089dbe5e177245bc57cf679a3e68f45
Ariful Hasan Kawshiq <hasan.kawshiq@northsouth.edu>
2019-05-01 18:41:41 +0600

    Update MainActivity.java

--- Project Codes/app/src/main/java/it/kawshiq/tic_tac_toeK/MainActivity.java
+++ Project Codes/app/src/main/java/it/kawshiq/tic_tac_toeK/MainActivity.java
@@ -1,1 +1,33 @@
+package it.kawshiq.tic_tac_toeK;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+    private final int FP = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+    private  Button button;
+    private  Button useAIButton;
+    private TableLayout mTableLayout;
+    private TextView textView;
+    private Board gameBoard;
+
+    public static int counter= 1;
+
+    private boolean CPUCtrl = false;
+
+
+
+}
 
